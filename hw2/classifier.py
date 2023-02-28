326bff5089a460424613fa8166debf8093faf7c5
peter <o.peter612@gmail.com>
2023-02-28 15:00:29 -0600

    Adding comments, expanding variable names, adding new functions

--- hw2/classifier.py
+++ hw2/classifier.py
@@ -1,17 +1,23 @@
-from nltk.lm.preprocessing import pad_both_ends, flatten, padded_everygram_pipeline
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
+from nltk.lm.preprocessing import flatten, pad_both_ends, padded_everygram_pipeline
 from nltk.lm import MLE
-import sys, string
 
-words = ['a b c','b','c','d','e','f','g']
+import string
+
+# from nltk.tokenize import word_tokenize
 
 
 def data_preprocessing(authorlist):
+    # Declare necessary variables 
+    clean_words = {}
+
+    # Open the file indicating which text files to read in
     f = open(authorlist, "r")
     file_names = f.readlines()
-    clean_words = {}
+    print("List of files we will be reading in: ", file_names)
+
+    # Read in each file and process it's data
     for name in file_names:
         sentences = []
         ftemp = open(name[:-1], 'r', encoding='utf-8')
@@ -21,40 +27,65 @@ def data_preprocessing(authorlist):
             real_lines += line
         sentences = real_lines.split('.')
 
+        # Clean the data 
         table = str.maketrans('', '', string.punctuation)
         stripped = []
         clean_sentences = []
-        # padded = []
+        padded = []
         for sentence in sentences[:-1]:
             stripped = sentence.translate(table)
             lower = stripped.lower()
             individual_sentence = lower.split()
             
+            # Remove non-alphabetic strings (i.e. punctuation) from the data
             clean_sentence = []
-            
-            sw = set(stopwords.words('english'))
-            for w in individual_sentence:
+            stop_words = set(stopwords.words('english'))
+            for word in individual_sentence:
                 alpha = "" 
-                for l in w:
-                    if l.isalpha():
-                        alpha += l
-                if alpha not in sw and alpha != "":
+                for index in word:
+                    if index.isalpha():
+                        alpha += index
+                if alpha != "":
                     clean_sentence += [alpha]
-                
             clean_sentences += [clean_sentence]
-        # padded = list(flatten(pad_both_ends(s, n=2) for s in clean_sentences))
+
+        clean_sentences = list(flatten(pad_both_ends(sent, n=2) for sent in clean_sentences))
         train, vocab = padded_everygram_pipeline(2, clean_sentences)
+
         ftemp.close()
-        clean_words[name[:-1]] = (train, vocab)
+        clean_words[name[:-1]] = (train, vocab, clean_sentences)
+    
     return clean_words
 
-def train(name, model):
+
+print("\nProcessing the data...")
+processed_data = data_preprocessing('authorlist.txt')
+
+
+# Our data has been processed and cleaned, now we train our model
+def model_training(name, model):
     lm = MLE(2)
-    lm.fit(model[0], model[1])
-    print(lm.vocab)
+
+    train = model[0]
+    vocab = model[1]
+    lm.fit(train, vocab)
+
+    return lm
 
 
-processed = data_preprocessing('authorlist.txt')
-for key, value in processed.items():
-    train(key, value)
-# print(processed)
+print("\nTraining the model...")
+model_list = []
+sentence_data = []
+
+for key, value in processed_data.items():
+    print(key)
+    model_list += [model_training(key, value)]
+    sentence_data += [value[2]]
+
+
+def use_model(model, sequence):
+    print(model.perplexity(sequence))
+    
+
+print("\nUsing the trained model...")
+use_model(model_list[0], [sentence_data[0], sentence_data[1]])
