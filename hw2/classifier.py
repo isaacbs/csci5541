484b7618e9b6a613919b3aee1e13eec4852edd1b
Isaac Blaine-Sauer <blain075@umn.edu>
2023-02-28 19:15:00 -0600

    adding functionality for -test flag, preprocessing of data, and model evaluation

--- hw2/classifier.py
+++ hw2/classifier.py
@@ -2,67 +2,105 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.lm.preprocessing import flatten, pad_both_ends, padded_everygram_pipeline
 from nltk.lm import MLE
+from nltk.util import bigrams
+from nltk.util import everygrams
 
-import string
+import string, sys
 
 
-def data_preprocessing(authorlist):
+def data_preprocessing(name, test):
     # Declare necessary variables 
     clean_words = {}
+    
+    sentences = []
+    ftemp = open(name[:-1], 'r', encoding='utf-8')
+    temp_lines = ftemp.readlines()
+    real_lines = ""
+    for line in temp_lines:
+        real_lines += line
+    sentences = real_lines.split('.')
 
-    # Open the file indicating which text files to read in
-    f = open(authorlist, "r")
-    file_names = f.readlines()
-    print("List of files we will be reading in: ", file_names)
+    # Clean the data 
+    table = str.maketrans('', '', string.punctuation)
+    stripped = []
+    clean_sentences = []
+    for sentence in sentences[:-1]:
+        stripped = sentence.translate(table)
+        lower = stripped.lower()
+        individual_sentence = lower.split()
 
-    # Read in each file and process it's data
-    for name in file_names:
-        sentences = []
-        ftemp = open(name[:-1], 'r', encoding='utf-8')
-        temp_lines = ftemp.readlines()
-        real_lines = ""
-        for line in temp_lines:
-            real_lines += line
-        sentences = real_lines.split('.')
+        # Remove non-alphabetic strings (i.e. punctuation) from the data
+        clean_sentence = []
+        for word in individual_sentence:
+            alpha = "" 
+            for index in word:
+                if index.isalpha():
+                    alpha += index
+            if alpha != "":
+                clean_sentence += [alpha]
+        clean_sentences += [clean_sentence]
+
+    ftemp.close()
+
+    # If test flag is present, then we want to train model on all data, if not, we need to break data into a training and dev set.
+    if test:
+        train, vocab = padded_everygram_pipeline(3, clean_sentences)
+        return [(train, vocab), []]
+    else:
+        # Break data into train and dev set
+        clean_length = len(clean_sentences)//10
+        train_set = clean_sentences[clean_length+1:]
+
+        #train model on train set
+        train, vocab = padded_everygram_pipeline(3, train_set)
+
+        #separate dev set and create everygrams based on each sentence
+        dev_set = clean_sentences[:clean_length]
+        every = []
+        for sentence in dev_set:
+            padded_trigrams = list(pad_both_ends(sentence, n=3))
+            every += [list(everygrams(padded_trigrams, max_len=3))]
+        return [(train, vocab), every]
+
+
+def test_data_processing(name):
+    # Declare necessary variables 
+
+    ftemp = open(name, 'r', encoding='utf-8')
+    temp_lines = ftemp.readlines()
+    table = str.maketrans('', '', string.punctuation)
+    clean_sentences = []
+    every = []
+
+    for line in temp_lines:
+        stripped = []
 
         # Clean the data 
-        table = str.maketrans('', '', string.punctuation)
-        stripped = []
-        clean_sentences = []
-        padded = []
-        for sentence in sentences[:-1]:
-            stripped = sentence.translate(table)
-            lower = stripped.lower()
-            individual_sentence = lower.split()
-            
-            # Remove non-alphabetic strings (i.e. punctuation) from the data
-            clean_sentence = []
-            stop_words = set(stopwords.words('english'))
-            for word in individual_sentence:
-                alpha = "" 
-                for index in word:
-                    if index.isalpha():
-                        alpha += index
-                if alpha != "":
-                    clean_sentence += [alpha]
-            clean_sentences += [clean_sentence]
+        stripped = line.translate(table)
+        lower = stripped.lower()
+        individual_sentence = lower.split()
 
-        clean_sentences = list(flatten(pad_both_ends(sent, n=2) for sent in clean_sentences))
-        train, vocab = padded_everygram_pipeline(2, clean_sentences)
-
-        ftemp.close()
-        clean_words[name[:-1]] = (train, vocab, clean_sentences)
+        # Remove non-alphabetic strings (i.e. punctuation) from the data
+        clean_sentence = []
+        for word in individual_sentence:
+            alpha = "" 
+            for index in word:
+                if index.isalpha():
+                    alpha += index
+            if alpha != "":
+                clean_sentence += [alpha]
+        clean_sentences += [clean_sentence]
     
-    return clean_words
+    for sentence in clean_sentences:
+        padded_trigrams = list(pad_both_ends(sentence, n=3))
+        every += [list(everygrams(padded_trigrams, max_len=3))]
 
-
-print("\nProcessing the data...")
-processed_data = data_preprocessing('authorlist.txt')
-
+    ftemp.close()
+    return every 
 
 # Our data has been processed and cleaned, now we train our model
-def model_training(name, model):
-    lm = MLE(2)
+def model_training(model, order):
+    lm = MLE(order)
 
     train = model[0]
     vocab = model[1]
@@ -70,20 +108,45 @@ def model_training(name, model):
 
     return lm
 
+def use_model(model, sequence):
+    print(model.perplexity(sequence))
 
+# Check for command line arguments. If there is more than just an authorlist present, then check if the second argument is the test flag. If it is, train the model fully on all of the data in the authorfile and output the classification result for each line in the testfile.
+if len(sys.argv) == 2:
+    f = open(sys.argv[1], "r")
+    file_names = f.readlines()
+    print("List of files we will be reading in: ", file_names)
+
+    models = {}
+    dev_data = {}
+    for text in file_names:
+        data_package = data_preprocessing(text, False)
+        models[text[:-1]] = model_training(data_package[0], 3)
+        dev_data[text[:-1]] = data_package[1]
+    print(dev_data)
+
+
+
+# Open the file indicating which text files to read in
+
+
+# Read in each file and process it's data
+
+
+
+
+
+'''
 print("\nTraining the model...")
 model_list = []
 sentence_data = []
 
 for key, value in processed_data.items():
     print(key)
-    model_list += [model_training(key, value)]
-    sentence_data += [value[2]]
+    print(value[0])
+    model_list += [model_training(key, value, 3)]
+    sentence_data += value[2]
+#     print(sentence_data)
 
 
-def use_model(model, sequence):
-    print(model.perplexity(sequence))
-    
-
-print("\nUsing the trained model...")
-use_model(model_list[0], [sentence_data[0], sentence_data[1]])
+'''
